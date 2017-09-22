62ae8b17f9cccf6768dcdb17bb32935b3b0a7f8f
Saumyakanta Sahoo <somu1795@gmail.com>
2017-09-22 12:30:42 +0530

    Create most_common.py

---
+++ most_common.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+# This is a solution to the problem on Hackerrand - https://www.hackerrank.com/challenges/most-commons/problem
+
+import operator
+
+s = input().strip()
+alpha = dict(zip([chr(i) for i in range(97, 123)], [x for x in range(1, 27)]))
+result2 = {}
+for x in alpha.keys():
+    result2.update({x:s.count(x)})
+        
+result = sorted(result2.items(), key=operator.itemgetter(1))
+result = sorted(result, key=lambda x: (-x[1],x[0]))
+result3 = result[:3]
+max1, num1 = result3[0]
+max2, num2 = result3[1]
+max3, num3 = result3[2]
+print(max1, num1)
+print(max2, num2)
+print(max3, num3)
